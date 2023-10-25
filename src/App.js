09cc2553b32be6939718a83464b62670da3cbd20
Ibtissam <btissam1234btissam12@gmail.com>
2023-10-25 15:43:54 +0100

    somtngs

--- src/App.js
+++ src/App.js
@@ -2,132 +2,40 @@ import './App.css';
 import Home from './home/Home';
 import Footer from './home/Footer';
 import Skills from './skills/Skills';
-import Project from './project/Project';
-import Contact from './Forms/Contact';
+import Contact from './Forms/Contact'; 
 import SkillsForm from './Forms/SkillsForm';
 import ProjectForm from './Forms/ProjectForm';
+import Project from './project/Project'
+import { useEffect, useState } from 'react';
 
 function App() {
-    const skillsData = [
-        {
-            category: "Frontend",
-            skills: [
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "../images/me.png'",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                // ... other frontend skills
-            ]
-        },
-        {
-            category: "Frontend",
-            skills: [
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                // ... other frontend skills
-            ]
-        },
-        {
-            category: "Frontend",
-            skills: [
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                // ... other frontend skills
-            ]
-        },
-        {
-            category: "Frontend",
-            skills: [
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-                {
-                    _id: "1",
-                    image: "/path/to/image1.png",
-                    name: "React"
-                },
-             
-            ]
-        },
-    ];
+    const [skills, setSkills] = useState([]);
+
+    const fetchSkills = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/skills', {
+                method: 'GET',
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch skills');
+            }
+
+            const data = await response.json(); 
+            setSkills(data); // Directly setting the array to the state
+        } catch (error) {
+            console.error("There was an error fetching the skills:", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchSkills();
+    }, []);
+
     const projectsData = [
         {
           id: "1",
-          image: "https://via.placeholder.com/150", // Placeholder image URL.
+          image: "https://via.placeholder.com/150",
           name: "Project A",
           technologie: "React, TailwindCSS",
           description: "This is a description for Project A. It showcases capabilities using React and TailwindCSS."
@@ -138,17 +46,17 @@ function App() {
           name: "Project B",
           technologie: "Node.js, Express",
           description: "Project B is developed using Node.js and Express. It's a backend service that provides various APIs."
-        },]
+        }
+    ];
 
     return (
         <>
             <Home />
-            <Skills skills={skillsData} />
-            <SkillsForm/>
-            <Project  projects={projectsData}/>
+            <Skills skills={skills} />
+            <SkillsForm />
+            <Project projects={projectsData} />
             <ProjectForm/>
             <Contact />
-
             <Footer />
         </>
     );
--- src/Forms/SkillsForm.js
+++ src/Forms/SkillsForm.js
@@ -1,26 +1,68 @@
-const SkillsForm = () => {
+import  { useState } from 'react';
+
+function SkillsForm() {
+    const [formData, setFormData] = useState({
+        category: '',
+        skills: { file: '', technologie: '' }
+    });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formDataObj = new FormData();
+        formDataObj.append('category', formData.category);
+        formDataObj.append('file', formData.skills.file);
+        formDataObj.append('technologie', formData.skills.technologie);
+
+        const res = await fetch('http://localhost:5000/skills', {
+            method: 'POST',
+            body: formDataObj
+        });
+
+        const data = await res.json();
+        console.log(data);
+    };
+
+    const handleInput = (e) => {
+        if (e.target.name === 'file') {
+            setFormData(prevState => ({
+                ...prevState,
+                skills: { ...prevState.skills, file: e.target.files[0] }
+            }));
+        } else if (e.target.name === 'category') {
+            setFormData(prevState => ({
+                ...prevState,
+                [e.target.name]: e.target.value
+            }));
+        } else if (e.target.name === 'technologie') {
+            setFormData(prevState => ({
+                ...prevState,
+                skills: { ...prevState.skills, technologie: e.target.value }
+            }));
+        }
+    };
+
     return (
         <div className="max-w-lg lg:ms-auto mx-auto text-center">
             <div className="py-16 px-7 rounded-md bg-white">
-                <form action="" method="POST">
+                <form onSubmit={handleSubmit} encType="multipart/form-data">
                     <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
-                    <div className="md:col-span-2">
-                            <label htmlFor="subject" className="float-left block font-normal text-gray-400 text-lg">category :</label>
-                            <select id="subject" name="subject" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-700">
-                                <option value=""  >Sélectionnez un domaine</option>
-                                <option value="" >Sélectionnez un domaine</option>
-                                <option value="" >Sélectionnez un domaine</option>
-                                <option value="" >Sélectionnez un domaine</option>
-                               
-                    </select>
-                    </div>
-                    <div className="md:col-span-2">
-                            <label htmlFor="file" className="float-left block font-normal text-gray-400 text-lg">technologie logo :</label>
-                            <input type="file" id="file" name="file" className="peer block w-full appearance-none border-none bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" />
-                        </div>
-                        
                         <div className="md:col-span-2">
-                            <input type="email" id="email" name="email" placeholder="technologie name" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-700" />
+                            <label htmlFor="category" className="float-left block font-normal text-gray-400 text-lg">category :</label>
+                            <select id="category" name="category" onChange={handleInput} className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-700">
+                                <option value="frontend">Frontend</option>
+                                <option value="backend">Backend</option>
+                                <option value="database">Database</option>
+                                <option value="devOps">DevOps</option>
+                            </select>
+                        </div>
+                        <div className="md:col-span-2">
+                            <label htmlFor="file" className="float-left block font-normal text-gray-400 text-lg">technologie logo :</label>
+                            <input type="file" id="file" name="file" onChange={handleInput} className="peer block w-full appearance-none border-none bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" />
+                        </div>
+                        <div className="md:col-span-2">
+                            <label htmlFor="technologie" className="float-left block font-normal text-gray-400 text-lg">technologie name:</label>
+                            <input type="text" id="technologie" value={formData.skills.technologie} onChange={handleInput} name="technologie" placeholder="technologie name" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-700" />
                         </div>
                         <div className="md:col-span-2">
                             <button className="py-3 text-base font-medium rounded text-white bg-blue-800 w-full hover:bg-blue-700 transition duration-300">Valider</button>
